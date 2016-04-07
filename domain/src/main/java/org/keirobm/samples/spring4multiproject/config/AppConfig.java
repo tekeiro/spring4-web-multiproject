2ca4e2e7f67b842ab71879eb6856af614af1da88
Angel Biedma <tekeiro@gmail.com>
2016-04-07 10:56:08 +0200

    Initial upload of domain and admin projects Initial gradle setup

---
+++ domain/src/main/java/org/keirobm/samples/spring4multiproject/config/AppConfig.java
@@ -0,0 +1,17 @@
+package org.keirobm.samples.spring4multiproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+/**
+ * Spring Beans Configuration
+ * Created by Angel Biedma on 07/04/2016.
+ */
+@Configuration
+@EnableWebMvc
+public class AppConfig {
+
+
+}
